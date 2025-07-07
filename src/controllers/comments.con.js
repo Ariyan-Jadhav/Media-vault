081f2f6d62d2d868cafbee039a6fbd25367a8d7a
Ariyan-Jadhav <om.jadhav_344@walchandsangli.ac.in>
2025-07-07 21:53:14 +0530

    Comment model completed, {updateComment, deleteComment updated}

--- src/controllers/comments.con.js
+++ src/controllers/comments.con.js
@@ -5,19 +5,61 @@ import mongoose from "mongoose";
 import { Comment } from "../models/comment.models.js";
 import { Video } from "../models/video.models.js";
 import { User } from "../models/user.models.js";
-import { getVideoById } from "./video.con.js";
 
 const getVideoComments = asyncHandle(async (req, res) => {
   //TODO: get all comments for a video
+
   const { page = 1, limit = 10 } = req.query;
+  const pageNum = Math.max(1, parseInt(page));
+  const limitNum = Math.min(50, Math.max(1, parseInt(limit)));
+  const skip = (pageNum - 1) * limitNum;
 
   const { videoId } = req.params;
   if (!videoId?.trim()) throw new apiError(200, "Video-Id not found");
 
   const video = await Video.findById(videoId);
-  if (!video) throw new apiError("Video not found");
+  if (!video) throw new apiError(404, "Video not found");
 
-  //aggregation work pending
+  const pipeline = [
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+  ];
+
+  const comments = await Comment.aggregate([
+    ...pipeline,
+    { $skip: skip },
+    { $limit: limitNum },
+  ]);
+  const totalComments = await Comment.aggregate([
+    ...pipeline,
+    { $count: "total" },
+  ]);
+
+  const totalCount = totalComments[0]?.total || 0;
+  const totalPage = Math.ceil(totalCount / limitNum);
+  const hasNextPage = pageNum < totalPage;
+  const hasPreviousPage = pageNum > 1;
+
+  return res.status(200).json(
+    new apiResponse(
+      200,
+      {
+        comments,
+        pagination: {
+          currentPage: pageNum,
+          limit: limitNum,
+          totalComments: totalCount,
+          totalPage,
+          hasNextPage,
+          hasPreviousPage,
+        },
+      },
+      "successfully comments fetched"
+    )
+  );
 });
 
 const addComment = asyncHandle(async (req, res) => {
@@ -33,18 +75,59 @@ const addComment = asyncHandle(async (req, res) => {
 
   const comment = await Comment.create({
     video: videoId,
-    owner: req.user._id,
+    user: req.user._id,
     content,
   });
+  //You can use mongoAgg but if user changes the title of the video or her username it will be not updated in the db
+  return res
+    .status(200)
+    .json(new apiResponse(200, comment, "successfully commented"));
+});
 
-  const populatedcomment = await Comment.findById(comment._id).populate({
-    path: "owner",
-    select: "fullName username avatar",
-  });
+const updateComment = asyncHandle(async (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId?.trim()) throw new apiError(500, "Comment-Id not found");
+
+  const { content } = req.body;
+  if (!content?.trim()) throw new apiError(500, "content not found");
+
+  const existingComment = await Comment.findById(commentId);
+  if (!existingComment) throw new apiError(404, "Comment not found");
+
+  if (existingComment.owner.toString() !== req.user._id?.toString())
+    throw new apiError(400, "Sorry you are not Sigma");
+
+  const comment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    { new: true }
+  );
+  return res
+    .status(200)
+    .json(new apiResponse(200, comment, "user comment updated"));
+});
+
+const deleteComment = asyncHandle(async (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId?.trim()) throw new apiError(400, "Comment-Id not found");
+
+  const comment = await Comment.findById(commentId);
+  if (!comment) throw new apiError(404, "comment not found");
+
+  if (comment.owner?.toString() !== req.user?._id.toString())
+    throw new apiError(403, "Sorry ur not Sigma");
+
+  await Comment.findByIdAndDelete(commentId);
 
   return res
     .status(200)
-    .json(new apiResponse(200, populatedcomment, "successfully commented"));
+    .json(
+      new apiResponse(200, { deletedId: commentId }, "comment deleted suckSex")
+    );
 });
 
-export { addComment };
+export { addComment, getVideoComments, updateComment, deleteComment };
--- src/routes/comment.rou.js
+++ src/routes/comment.rou.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.mid.js";
-import { addComment } from "../controllers/comments.con.js";
+import {
+  addComment,
+  deleteComment,
+  getVideoComments,
+  updateComment,
+} from "../controllers/comments.con.js";
 
 const commentRouter = Router();
 
 commentRouter.route("/add-com/:videoId").post(verifyJWT, addComment);
+commentRouter.route("/video-com/:videoId").get(verifyJWT, getVideoComments);
+commentRouter.route("/update-com/:commentId").post(verifyJWT, updateComment);
+commentRouter.route("/delete-com/:commentId").post(verifyJWT, deleteComment);
 
 export default commentRouter;
